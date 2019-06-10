edc3423775b6ee16a8975cb02e41d9a7413c58d7
Oana Berentan <oana.berentan@yahoo.com>
2019-06-10 20:26:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,14 @@
 class Anagram
   attr_accessor :word
 
-def initialize(word)
-  @word = word
-end
+ def initialize(word)
+   @word = word
+ end
 
-def match(%w(word))
-  self.%w(word).map{|w| w == w}
-end
+ def match(word_array)
+   word.array.select do |word|
+     word.split(" ").sort == @word.split(" ").sort
+   end
+ end
 
 end
